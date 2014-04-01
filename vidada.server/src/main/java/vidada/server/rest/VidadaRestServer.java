925dc030ec0ba1c76fa0e1a7475badb6ec01f8db
IsNull <isnull@securityvision.ch>
2014-04-01 19:24:19 +0200

    added example for static html in embedded Glassfish server

--- vidada.server/src/main/java/vidada/server/rest/VidadaRestServer.java
+++ vidada.server/src/main/java/vidada/server/rest/VidadaRestServer.java
@@ -1,5 +1,6 @@
 package vidada.server.rest;
 
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.servlet.FilterRegistration;
 import org.glassfish.grizzly.servlet.ServletRegistration;
@@ -30,26 +31,32 @@ public class VidadaRestServer {
 
     public HttpServer start() {
 
-        WebappContext webappContext = new WebappContext("Grizzly Web Context", "");
-
-        ServletRegistration servletRegistration = webappContext.addServlet(SERVLET_JERSEY, org.glassfish.jersey.servlet.ServletContainer.class);
-        servletRegistration.addMapping("/api/*");
-        servletRegistration.setInitParameter("jersey.config.server.provider.packages", "vidada.server.rest.resource");
-
-        // Add filters
-
-        // Basic Http Authentication filter
-        FilterRegistration filter = webappContext.addFilter("Auth filter", new BasicHttpAuthFilter("admin", "1337"));
-        filter.addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), true, SERVLET_JERSEY);
-
-
         // Create the Web server
 
         HttpServer server = HttpServer.createSimpleServer("http://0.0.0.0/", 5555);
 
+        // Register a REST handler
+        WebappContext webappContext = createRESTContext();
         webappContext.deploy(server);
-        //server.getServerConfiguration().addHttpHandler(new StaticHttpHandler("/var/www/webpage/"), "/xyz");
-        server.getServerConfiguration().addHttpHandler(new MediaStreamHttpHandler(vidadaServer.getMediaService()), "/stream");
+
+        // Register a custom Stream handler
+        server.getServerConfiguration().addHttpHandler(
+                new MediaStreamHttpHandler(vidadaServer.getMediaService()),
+                "/stream"); // map the stream handler to /stream
+
+        // Register a static http handler for this jar
+        server.getServerConfiguration().addHttpHandler(
+                new CLStaticHttpHandler(
+                       this.getClass().getClassLoader(), // ClassLoader of the JAR
+                       "/www/"),    // absolute path in the jar (see src/main/resources/www)
+                "/");               // map the default http to the root
+
+
+        /* Example for specific directory outside of jar
+
+                // Register a static http handler for a directory
+                server.getServerConfiguration().addHttpHandler(new StaticHttpHandler("/var/www/webpage/"), "/xyz");
+         */
 
         try {
             server.start();
@@ -58,6 +65,22 @@ public class VidadaRestServer {
         }
 
         return server;
+    }
+
+    private WebappContext createRESTContext(){
+        WebappContext webappContext = new WebappContext("Grizzly Web Context", "");
+
+        ServletRegistration servletRegistration = webappContext.addServlet(SERVLET_JERSEY, org.glassfish.jersey.servlet.ServletContainer.class);
+        servletRegistration.addMapping("/api/*"); // map the REST API to the /api
+        servletRegistration.setInitParameter("jersey.config.server.provider.packages", "vidada.server.rest.resource"); // scan this package for resource classes
+
+        // Add filters
+
+        // Basic Http Authentication filter
+        FilterRegistration filter = webappContext.addFilter("Auth filter", new BasicHttpAuthFilter("admin", "1337")); // TODO replace with real credential system
+        filter.addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), true, SERVLET_JERSEY);
+
+        return webappContext;
     }
 
 }
