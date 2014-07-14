325a766764fa339bbc9311c329c79d30fb3c8fc6
IsNull <isnull@securityvision.ch>
2014-07-14 21:12:04 +0200

    ThreadUtil - added runUIThreadWait

--- vidada.client/src/main/java/vidada/client/ThreadUtil.java
+++ vidada.client/src/main/java/vidada/client/ThreadUtil.java
@@ -1,11 +1,21 @@
 package vidada.client;
 
 import javafx.application.Platform;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Utility to run code in desired threads.
  */
 public abstract class ThreadUtil {
+
+    transient private static final Logger logger = LogManager.getLogger(ThreadUtil.class.getName());
+
 
     private static ThreadUtil instance;
 
@@ -19,25 +29,33 @@ public abstract class ThreadUtil {
     /**
      * Runs the the given code in the UI thread.
      * If called form the UI thread, it will execute the code right away.
-     * @param update
+     * @param code
      */
-    public static void runUIThread(Runnable update){
-        instance().runUIThreadInternal(update);
+    public static void runUIThread(Runnable code){
+        instance().runUIThreadInternal(code);
     }
+
+    /**
+     * Runs the the given code in the UI thread and waits (blocks) until it has completed.
+     * @param code
+     */
+    public static void runUIThreadWait(Runnable code) { instance().runUIThreadWaitInternal(code);}
 
     /**
      * Runs the the given code in the UI thread.
      * If called form the UI thread, it will execute the code right away.
      *
-     * @param update
+     * @param code
      */
-    public abstract void runUIThreadInternal(Runnable update);
+    public abstract void runUIThreadInternal(Runnable code);
 
+    public abstract void runUIThreadWaitInternal(Runnable code);
 
     /**
      * JavaFX implementation
      */
     private static class ThreadUtilFx extends ThreadUtil {
+
         @Override
         public void runUIThreadInternal(Runnable update) {
             if(!Platform.isFxApplicationThread()){
@@ -46,6 +64,54 @@ public abstract class ThreadUtil {
                 update.run();
             }
         }
+
+        @Override
+        public void runUIThreadWaitInternal(final Runnable code) {
+            if (Platform.isFxApplicationThread()) {
+                code.run();
+            } else {
+                final Lock lock = new ReentrantLock();
+                final Condition condition = lock.newCondition();
+                final ThrowableWrapper throwableWrapper = new ThrowableWrapper();
+                lock.lock();
+                try {
+                    Platform.runLater(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            lock.lock();
+                            try {
+                                code.run();
+                            } catch (Throwable e) {
+                                throwableWrapper.t = e;
+                            } finally {
+                                try {
+                                    condition.signal();
+                                } finally {
+                                    lock.unlock();
+                                }
+                            }
+                        }
+                    });
+                    condition.await();
+                    if (throwableWrapper.t != null) {
+                        throw new ExecutionException(throwableWrapper.t);
+                    }
+                } catch (InterruptedException e) {
+                    logger.error(e);
+                } catch (ExecutionException e) {
+                    logger.error(e);
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
     }
+
+
+    private static class ThrowableWrapper {
+        Throwable t;
+    }
+
 
 }
